9f549bdfb5514e978e3c37c6b54a9f371258ee45
UZAIR <108986693+UZAIR78920@users.noreply.github.com>
2025-02-25 12:10:05 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,38 @@
+// ALGORITHM SelectionSort(A,n)
+//     Input: A - array of n elements
+//     Output: Sorted array A
+//     for i ← 0 to n - 2 do
+//         minIndex ← i
+//         for j ← i + 1 to n - 1 do
+//             if A[j] < A[minIndex] then
+//                 minIndex ← j
+//         Swap A[i] and A[minIndex] 
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.println("Unsorted Array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        selectionSort(arr);
+        System.out.println("Sorted Array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
